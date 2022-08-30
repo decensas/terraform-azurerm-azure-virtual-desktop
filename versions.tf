cd6528261fffe329ee149e34a27a42c32d91ec07
Sindre Korsnes <sindre@decens.no>
2022-08-30 09:51:48 +0200

    Add comment about TF core version

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.2.8"
+  required_version = ">= 1.2.8" # Also update terraform version in .github/workflows/pre-commit.yml
 
   required_providers {
     azurerm = {
