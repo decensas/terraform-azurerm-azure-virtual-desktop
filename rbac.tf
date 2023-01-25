0348ae7171215ea50fde864d5a56306dd85316ee
Sindre Korsnes <55540102+sindkor@users.noreply.github.com>
2023-01-25 10:00:48 +0100

    Add note about AVD SP ID

--- rbac.tf
+++ rbac.tf
@@ -6,6 +6,10 @@ data "azuread_users" "admins" {
   user_principal_names = var.avd_admins_upns
 }
 
+# This is the service principal for the Microsoft-managed AVD application
+# It will vary depending on when your tenant first registered for AVD,
+# But its application ID will always be the one below, see:
+# https://learn.microsoft.com/en-us/azure/virtual-desktop/start-virtual-machine-connect?tabs=azure-portal
 data "azuread_service_principal" "avd" {
   application_id = "9cdead84-a844-4324-93f2-b2e6bb768d07"
 }
